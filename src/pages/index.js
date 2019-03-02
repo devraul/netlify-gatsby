c679a4df1f4b28411564b576c3e66f24932cfb38
Raul de Melo <melo.raulf@gmail.com>
2019-03-02 12:13:23 +0100

    Centralize content, add link references to the article and change initial text

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,52 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Olar pessoas</h1>
-    <p>Bem vindo ao meu site em Gastsby.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <main
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>Olar pessoas</h1>
+      <p>
+        Este é um projeto com objetivo de mostrar como é feito um deploy com
+        Netlify!{" "}
+        <span role="img" aria-label="emoji feliz">
+          😄
+        </span>
+      </p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+
+      <div>
+        <h2>Links</h2>
+        <ul>
+          <li>
+            <a
+              href="https://github.com/devraul/netlify-gatsby"
+              target="_blank"
+              without
+              rel="noopener noreferrer"
+            >
+              Link do repositório
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://medium.com/@raul_fdm/subindo-seu-projeto-frontend-em-2019-netlify-c52e15574736"
+              target="_blank"
+              without
+              rel="noopener noreferrer"
+            >
+              Link do Artigo
+            </a>
+          </li>
+        </ul>
+      </div>
+    </main>
   </Layout>
 )
 
